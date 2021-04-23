515ad859eb440d46ae8b98dbf199a4f65c7c49f9
calypsorrr <74073146+calypsorrr@users.noreply.github.com>
2021-04-23 12:00:15 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,21 +8,28 @@ import sensor
 i2c = I2C(0, I2C.MASTER)
 si7021 = SI7021(i2c)
 
-relais = Pin('P21', mode=Pin.OUT)
+relais = Pin('P2', mode=Pin.OUT)
 
 while True:
-	humidity = si7021.humidity()
-	temperature = si7021.temperature()
-	print(str(temperature) + " celcius")
-	print(str(humidity) + " %")
-	print("")
-	if humidity <= 70:
-		relais.value(1)
-	else:
-		relais.value(0)
-	time.sleep(1)
+	global humidity
+	global temperature
+	try:
+		humidity = si7021.humidity()
+		temperature = si7021.temperature()
+		print(str(temperature) + " celcius")
+		print(str(humidity) + " %")
+		print("")
+		if humidity <= 70:
+			relais.value(1)
+		else:
+			relais.value(0)
+		time.sleep(1)
+	except OSError as e:
+		print(e)
+		time.sleep(2)
 
 	sensor.readuart()
 	distance = sensor.readout()
 	if distance != False:
+		print("inside me!")
         wifiada.sendDataWifi(distance)
